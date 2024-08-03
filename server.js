a9c1a39de26439043c45299ed7fc5a20bd430dab
Satya Sai Teja Modalavalasa <imsatyasaiteja@gmail.com>
2024-08-03 19:16:34 +0000

    First Commit

--- server.js
+++ server.js
@@ -7,18 +7,32 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
+const helmet = require('helmet');
+const bcrypt = require('bcrypt'); 
 
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: 'DENY'}));
+app.use(helmet.xssFilter({}));
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+var ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({directives : {defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com']}}));
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res);
+    });
+});
 //END_ASYNC
 
 //START_SYNC
-
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
 //END_SYNC
 
 
